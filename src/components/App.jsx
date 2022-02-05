1f1e2a3d7e0e72ee9ad6a2f4092a58736a5c8807
Archishman-Das <sumita123prl@gmail.com>
2022-02-06 02:11:37 +0530

    Keeper App - Part 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
+import notes from "../notes";
 
 function App()
 {
     return (
         <div>
             <Header />
-            <Note />
+            {notes.map(note => (<Note id={note.id} title={note.title} content={note.content} />) )}
             <Footer />
         </div>
     )
---
+++ src/notes.js
@@ -0,0 +1,14 @@
+const notes=[
+    {
+        id: 1,
+        title: "First note",
+        content: "Blah blah blah"
+    },
+    {
+        id: 2,
+        title: "Second note",
+        content: "Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum"
+    }
+];
+
+export default notes;
